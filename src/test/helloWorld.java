f158c80808e9bf2b8bdc9ca3130ed391caee165d
sonichen <57282299+sonichen@users.noreply.github.com>
2020-12-07 17:53:47 +0800

    first commit

---
+++ src/test/helloWorld.java
@@ -0,0 +1,9 @@
+package test;
+
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("使用Git");
+    }
+}
+
